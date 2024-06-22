1d5dc6749be198ad8ef6fd477cea7074337a73dd
jared <jared_solano@protonmail.com>
2024-06-22 15:08:52 +0100

    trig plotting

---
+++ trigonometry/main.py
@@ -0,0 +1,36 @@
+import math
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def main():
+    X = np.arange(-1 * math.pi * 4, math.pi * 4, 0.05)
+
+    tan = np.tan(X)
+    sin = np.sin(X)
+    cos = np.cos(X)
+
+    cot = 1 / np.tan(X)
+    sec = 1 / np.cos(X)
+    cosec = 1 / np.sin(X)
+
+    plt.plot(X, tan, color="C0", label="tan(x)")
+    plt.plot(X, sin, color="C1", label="sin(x)")
+    plt.plot(X, cos, color="C2", label="cos(x)")
+
+    plt.plot(X, cot, color="C3", label="cot(x)")
+    plt.plot(X, sec, color="C4", label="sec(x)")
+    plt.plot(X, cosec, color="C5", label="cosec(x)")
+
+    ax = plt.gca()
+    ax.set_ylim([-2, 2])
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.grid(axis="x")
+    plt.legend()
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
