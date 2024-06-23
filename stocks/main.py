aa6f6ea75f66496922902291a4ab1cb06343ebda
jared <jared_solano@protonmail.com>
2024-06-23 20:27:30 +0100

    graphing for stock prices

---
+++ stocks/main.py
@@ -0,0 +1,49 @@
+import os
+
+import matplotlib.dates as mdates
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def main():
+    add_data_for_set("./stocks/data/apple.csv", "C0")
+    add_data_for_set("./stocks/data/nvidia.csv", "C1")
+    add_data_for_set("./stocks/data/alphabet_a.csv", "C2")
+    add_data_for_set("./stocks/data/amd.csv", "C3")
+    add_data_for_set("./stocks/data/microsoft.csv", "C4")
+
+    ax = plt.gca()
+    ax.xaxis.set_major_locator(mdates.MonthLocator(interval=3))
+    ax.xaxis.set_major_formatter(mdates.DateFormatter("%Y-%m"))
+
+    plt.xlabel("Date")
+    plt.ylabel("Closing Price ($)")
+    plt.xticks(rotation=90)
+    plt.title("Historical Stock Price")
+
+    plt.legend()
+    plt.show()
+
+
+def add_data_for_set(file, colour):
+    data = pd.read_csv(file)
+
+    # Clean data set
+    data["Date"] = pd.to_datetime(data["Date"])
+    data["Close/Last"] = (
+        data["Close/Last"].replace("[$,]", "", regex=True).astype(float)
+    )
+    data["Close/Last"] = pd.to_numeric(data["Close/Last"])
+
+    label = get_filename_without_path_and_extension(file)
+    plt.plot(data["Date"], data["Close/Last"], color=colour, label=label)
+
+
+def get_filename_without_path_and_extension(file_path):
+    filename = os.path.basename(file_path)
+    filename_without_extension = os.path.splitext(filename)[0]
+    return filename_without_extension
+
+
+if __name__ == "__main__":
+    main()
